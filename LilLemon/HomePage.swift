1ff2705b432be0db8eab95fc3c94d0fa4084a071
juliShmi <juliashmitko@gmail.com>
2024-06-12 22:56:47 +0300

    Add onboarding and User profile views

---
+++ LilLemon/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  LilLemon
+//
+//  Created by Iuliia Shmitko on 12/6/24.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
---
+++ LilLemon/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  LilLemon
+//
+//  Created by Iuliia Shmitko on 12/6/24.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}
